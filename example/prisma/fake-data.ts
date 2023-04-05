e370ab4d42a505a72bb5290a6ef8dbd00813febc
Luis Rudge <luis@luisrudge.net>
2023-04-04 20:39:20 -0600

    feat: remove overrides and add fake*Complete methods with foreign keys

--- example/prisma/fake-data.ts
+++ example/prisma/fake-data.ts
@@ -3,9 +3,7 @@ import { faker } from '@faker-js/faker';
 import { fakeSettings } from './fakeData.utils';
 export * from './fakeData.utils';
 
-export function fakeUser(
-  overrides?: Partial<Omit<Prisma.UserUncheckedCreateInput, ''>>,
-) {
+export function fakeUser() {
   return {
     email: faker.internet.email(),
     name: faker.name.fullName(),
@@ -13,6 +11,40 @@ export function fakeUser(
     settings: fakeSettings(),
     maybeString: faker.datatype.boolean() ? undefined : faker.lorem.words(5),
     status: faker.helpers.arrayElement(['active', 'inactive'] as const),
-    ...overrides,
+  };
+}
+export function fakeUserComplete() {
+  return {
+    id: faker.datatype.uuid(),
+    email: faker.internet.email(),
+    name: faker.name.fullName(),
+    age: faker.datatype.number({ min: 0, max: 99 }),
+    settings: fakeSettings(),
+    maybeString: faker.datatype.boolean() ? undefined : faker.lorem.words(5),
+    status: faker.helpers.arrayElement(['active', 'inactive'] as const),
+  };
+}
+export function fakeProfile() {
+  return {
+    someConfiguration: faker.datatype.boolean(),
+  };
+}
+export function fakeProfileComplete() {
+  return {
+    id: faker.datatype.number(),
+    someConfiguration: faker.datatype.boolean(),
+    userId: faker.datatype.uuid(),
+  };
+}
+export function fakeProfileSettings() {
+  return {
+    someConfiguration: faker.datatype.boolean(),
+  };
+}
+export function fakeProfileSettingsComplete() {
+  return {
+    id: faker.datatype.number(),
+    someConfiguration: faker.datatype.boolean(),
+    profileId: faker.datatype.number(),
   };
 }
--- src/helpers/createMethods.ts
+++ src/helpers/createMethods.ts
@@ -1,7 +1,12 @@
 import { logger } from '@prisma/internals';
 import { DMMF } from '@prisma/generator-helper';
 
-function getFieldDefinition(field: DMMF.Field, enums: DMMF.DatamodelEnum[]) {
+function getFieldDefinition(
+  models: DMMF.Model[],
+  model: DMMF.Model,
+  field: DMMF.Field,
+  enums: DMMF.DatamodelEnum[],
+) {
   let nullHandling = '';
   if (!field.isRequired) {
     nullHandling = `faker.datatype.boolean() ? undefined : `;
@@ -19,6 +24,20 @@ function getFieldDefinition(field: DMMF.Field, enums: DMMF.DatamodelEnum[]) {
         .join(', ');
       return `${field.name}: ${nullHandling}faker.helpers.arrayElement([${enumValuesAsString}] as const)`;
     }
+  }
+  if (field.isId) {
+    return `${field.name}: ${nullHandling}${
+      field.type === 'String'
+        ? 'faker.datatype.uuid()'
+        : 'faker.datatype.number()'
+    }`;
+  }
+  if (model.fields.some((it) => it.relationFromFields?.includes(field.name))) {
+    return `${field.name}: ${nullHandling}${
+      field.type === 'String'
+        ? 'faker.datatype.uuid()'
+        : 'faker.datatype.number()'
+    }`;
   }
   if (field.type === 'String') {
     if (field.isList) {
@@ -102,40 +121,8 @@ export async function createMethods(
   const functions: string[] = [];
 
   models.forEach((m) => {
-    const validFields = m.fields
-      .filter((field) => !field.isId)
-      .filter((field) => field.kind === 'scalar' || field.kind === 'enum')
-      .filter((field) => {
-        return !m.fields.find((it) => {
-          return it.relationFromFields?.includes(field.name);
-        });
-      })
-      .filter((field) => !field.hasDefaultValue)
-      .map((f) => getFieldDefinition(f, enums))
-      .filter(Boolean);
-
-    const relationFields = m.fields
-      .filter((field) => {
-        return m.fields.find((it) => {
-          return it.relationFromFields?.includes(field.name);
-        });
-      })
-      .map((f) => f.name);
-    const omitFields =
-      relationFields.length > 0
-        ? `, ${relationFields.map((it) => `'${it}'`).join(' | ')}`
-        : `, ''`;
-
-    functions.push(
-      `export function fake${m.name}(overrides?: Partial<Omit<Prisma.${
-        m.name
-      }UncheckedCreateInput${omitFields}>>) {
-  return {
-    ${validFields.join(',\n    ')}${validFields.length > 0 ? ',' : ''}
-    ...overrides,
-  };
-}`,
-    );
+    createFakeFunctionsWithoutFKs(models, m, enums, functions);
+    createFakeFunctionsWithFKs(models, m, enums, functions);
   });
   return await `import type { Prisma } from '@prisma/client';
 import { faker } from '@faker-js/faker';
@@ -144,4 +131,52 @@ ${extraExport || ''}
 
 ${functions.join('\n')}
 `;
+}
+function createFakeFunctionsWithoutFKs(
+  models: DMMF.Model[],
+  model: DMMF.Model,
+  enums: DMMF.DatamodelEnum[],
+  functions: string[],
+) {
+  const validFields = model.fields
+    .filter((field) => !field.isId)
+    .filter((field) => field.kind === 'scalar' || field.kind === 'enum')
+    .filter((field) => {
+      return !model.fields.find((it) => {
+        return it.relationFromFields?.includes(field.name);
+      });
+    })
+    .filter((field) => !field.hasDefaultValue)
+    .map((f) => getFieldDefinition(models, model, f, enums))
+    .filter(Boolean);
+  if (validFields.length > 0) {
+    functions.push(
+      `export function fake${model.name}() {
+  return {
+    ${validFields.join(',\n    ')},
+  };
+}`,
+    );
+  }
+}
+
+function createFakeFunctionsWithFKs(
+  models: DMMF.Model[],
+  model: DMMF.Model,
+  enums: DMMF.DatamodelEnum[],
+  functions: string[],
+) {
+  const validFields = model.fields
+    .filter((field) => field.kind === 'scalar' || field.kind === 'enum')
+    .map((f) => getFieldDefinition(models, model, f, enums))
+    .filter(Boolean);
+  if (validFields.length > 0) {
+    functions.push(
+      `export function fake${model.name}Complete() {
+  return {
+    ${validFields.join(',\n    ')},
+  };
+}`,
+    );
+  }
 }
