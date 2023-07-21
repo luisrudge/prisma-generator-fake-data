2f879a21025d26e5bd57713c3fbe8b18a0451f7f
Luis Rudge <luis@luisrudge.net>
2023-07-21 15:35:06 -0600

    feat: upgrade to prisma 5

--- src/generator.ts
+++ src/generator.ts
@@ -2,6 +2,7 @@ import { generatorHandler, GeneratorOptions } from '@prisma/generator-helper';
 import { GENERATOR_NAME } from './constants';
 import { createMethods } from './helpers/createMethods';
 import { writeFileSafely } from './utils/writeFileSafely';
+import invariant from 'tiny-invariant';
 
 const { version } = require('../package.json');
 generatorHandler({
@@ -14,11 +15,27 @@ generatorHandler({
     };
   },
   async onGenerate(options: GeneratorOptions) {
+    invariant(
+      typeof options.generator.config.extraExport === 'string' ||
+        options.generator.config.extraExport === undefined,
+      'extraExport must be a string or empty',
+    );
+    invariant(
+      typeof options.generator.config.extraExport === 'string' ||
+        options.generator.config.extraExport === undefined,
+      'extraExport must be a string or empty',
+    );
+    invariant(
+      typeof options.generator.config.emptyValueAs === 'string' ||
+        options.generator.config.emptyValueAs === undefined,
+      'emptyValueAs must be a string or empty',
+    );
+
     const fakeMethods = await createMethods(
       options.dmmf.datamodel,
-      options.generator.config.extraImport,
-      options.generator.config.extraExport,
-      options.generator.config.emptyValueAs,
+      options.generator.config.extraImport as string | undefined,
+      options.generator.config.extraExport as string | undefined,
+      options.generator.config.emptyValueAs as string | undefined,
     );
 
     await writeFileSafely(options.generator.output?.value!, fakeMethods);
