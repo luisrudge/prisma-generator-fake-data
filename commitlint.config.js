47412a75ead6026c477b99994b6c2cdb610e0654
Luis Rudge <luis@luisrudge.net>
2023-11-23 11:09:05 -0700

    commitlint

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,3 +1,13 @@
 module.exports = {
   extends: ['@commitlint/config-conventional'],
+  plugins: {
+    '@release-it/conventional-changelog': {
+      gitRawCommitsOpts: {
+        merges: null,
+      },
+      parserOpts: {
+        mergePattern: '^Merge pull request #(\\d+) from (.*)$',
+      },
+    },
+  },
 };
