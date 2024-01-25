0ff142467ea4a96195ed4b9528c531a4c3c70d92
Luis Rudge <luis@luisrudge.net>
2024-01-25 13:45:09 -0700

    fix: Correctly generate import path

--- src/utils/generatorUtils.ts
+++ src/utils/generatorUtils.ts
@@ -1,9 +1,19 @@
 import { GeneratorOptions } from '@prisma/generator-helper';
 
 export function extractClientPath(options: GeneratorOptions) {
-  return (
-    options.otherGenerators.find(
-      (g) => g?.provider?.value === 'prisma-client-js',
-    )?.output?.value || undefined
-  );
+  const clientPath = options.otherGenerators.find(
+    (g) => g?.provider?.value === 'prisma-client-js',
+  )?.output?.value;
+
+  /**
+   * not sure what changed, but this value was undefined before
+   * and now it's set to the full path of the client (including
+   * the user folder etc). If we detect that, we just return
+   * the @prisma/client string
+   */
+  if (clientPath?.includes('node_modules/@prisma/client')) {
+    return '@prisma/client';
+  }
+
+  return clientPath || undefined;
 }
